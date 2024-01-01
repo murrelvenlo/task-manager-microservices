c1ef3b2d797292766cb9abb2d99a7706cab5f203
murrelvenlo <r0781309@student.thomasmore.be>
2024-01-01 11:34:34 +0100

    uc update

--- user-service/src/main/java/fact/it/userservice/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package fact.it.userservice.service;
-
-import fact.it.userservice.dto.LoginRequest;
-import fact.it.userservice.dto.LoginResponse;
-import org.springframework.http.ResponseEntity;
-
-public interface AuthService {
-    ResponseEntity<LoginResponse> login(LoginRequest request);
-}
