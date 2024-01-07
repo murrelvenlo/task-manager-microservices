567d8c9ddccd622140a93bef35d072a33d846e95
murrelvenlo <r0781309@student.thomasmore.be>
2024-01-07 19:55:38 +0100

    reconfig auth.

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -1,27 +1,30 @@
-//package fact.it.apigateway.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-//        return http
-//                .authorizeExchange(exchange -> exchange
-//                        .pathMatchers("/api/member/get/all").permitAll()
-//                        .anyExchange().authenticated())
-//                .oauth2ResourceServer(oauth -> oauth.jwt(withDefaults()))
-//                .build();
-//    }
-//
-//}
+package fact.it.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.GET, "/api/members/get/all").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/api/members/add").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/teams/get/all").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/tasks/get/all").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth -> oauth.jwt(withDefaults()))
+                .build();
+    }
+
+}
